ce2f2a894918afe1cc174b02c16182914355ca87
Ai Rosa Rosita <rosa10221083@student.sttcipasung.ac.id>
2024-08-23 14:04:36 +0700

    form login and form sign up

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   resources :tests
   devise_for :users
   
+
+  devise_scope :user do
+     get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
